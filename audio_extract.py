555027a772b26792f0df5ad1263fd0d327d58117
C0MODIN <38263591+C0MODIN@users.noreply.github.com>
2021-01-30 12:48:56 -0300

    Update audio_extract.py
    first

--- audio_extract.py
+++ audio_extract.py
@@ -3,12 +3,19 @@ import sys
 import os
 import subprocess
 
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-'youtube_dl'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'youtube_dl'])
+
 
 def downloader(url):
-    os.system(f"youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 {url}")
+    os.system(
+        f"youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 {url}")
 
-if __name__ == "__main__":
-    url = (sys.argv[1])
-    downloader(url)
+
+try:
+
+    if __name__ == "__main__":
+        url = (sys.argv[1])
+        downloader(url)
+
+except:
+    print('Need URL!\nmp3-downloader [URL]')
